72baec2e3817437669bdbeb32466af168f36657d
Martin Trajanovski <martin.trajanovski@ess.eu>
2020-05-19 12:21:11 +0200

    move env variables to package json

--- config/configureAndValidate.js
+++ config/configureAndValidate.js
@@ -1,7 +1,3 @@
-const { config } = require('dotenv');
 const validateEnv = require('./validateEnv');
 
-config({
-  path: process.env.NODE_ENV === 'production' ? '.env' : '.env.development',
-});
 validateEnv();
