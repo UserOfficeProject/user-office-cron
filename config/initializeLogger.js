e133c68b8b00f52936a3b95997f3fb20305e5a6a
Jekabs Karklins <enthusiast.studio@gmail.com>
2023-10-09 22:43:18 +0200

    feat(initializeLogger.js): add debug log message when GrayLogLogger is initialized to provide information about server, port, and environment variables
    The changes were made to the `initializeLogger.js` file.
    
    - Added a debug log message using the `logger.logDebug` function to provide information about the initialization of the GrayLogLogger.
    - The log message includes the values of the `server`, `port`, and `env` variables.
    - This change was made to improve debugging and provide more visibility into the initialization process of the GrayLogLogger.

--- config/initializeLogger.js
+++ config/initializeLogger.js
@@ -2,12 +2,14 @@ const {
   setLogger,
   ConsoleLogger,
   GrayLogLogger,
+  logger,
 } = require('@user-office-software/duo-logger');
 
 const initializeLogger = () => {
   const server = process.env.GRAYLOG_SERVER;
   const port = process.env.GRAYLOG_PORT;
   const env = process.env.NODE_ENV || 'unset';
+
   setLogger([
     new ConsoleLogger(), // Log to console
     new GrayLogLogger( // Log to Graylog
@@ -17,6 +19,8 @@ const initializeLogger = () => {
       []
     ),
   ]);
+
+  logger.logDebug('GrayLogLogger initialized', { server, port, env });
 };
 
 module.exports = initializeLogger;
